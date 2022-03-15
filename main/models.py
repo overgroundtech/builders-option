5a330a5d9d0830a6fac04200bdc06bf0bef4c8d0
kimita <kimita@localhost.localdomain>
2022-03-15 15:36:09 +0300

    creating nav partial

--- main/models.py
+++ main/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_url(self):
+        return f'{self.name}-{self.id}'
+
 
 class Product(models.Model):
     categories = models.ManyToManyField(Category)
