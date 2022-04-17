a8324da92970f7d2507bb375ec60a6660f1cd2e5
kimita <kimita@localhost.localdomain>
2022-04-17 18:01:56 +0300

    added internal server error handler

--- main/views.py
+++ main/views.py
@@ -7,6 +7,10 @@ from django.contrib.auth.decorators import login_required
 from .models import *
 
 
+def server_error(request, exception):
+    return render(request, 'main/500.html', status=500)
+
+
 def page_not_found_view(request, exception):
     return render(request, 'main/404.html', status=404)
 
